4f14a20a4ef4c9cd32a3d1cf3e595ec51e7c886f
honghyobin <w2234@e-mirim.hs.kr>
2024-03-14 15:36:20 +0900

    node 03/14

--- ex01.js
+++ ex01.js
@@ -1,4 +1,4 @@
 console.log("node.js실행하기");
 파일명 = 'ex01'
-console.log('cmd에서 node'+파일명+'을 입력해봐요~');
-console.log(`cmd에서 node${파일명}'을 입력해봐요~`);
+console.log('cmd에서 node '+ 파일명+'을 입력해봐요~');
+console.log(`cmd에서 node ${파일명}'을 입력해봐요~`);
