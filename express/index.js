8f412f06c76507dee2918fb89ea166e785e18a18
honghyobin <w2234@e-mirim.hs.kr>
2024-04-11 15:49:11 +0900

    node 04/11

---
+++ express/index.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const app=express();
+const path = require('path')
+
+app.use("/html2",express.static(path.join(__dirname,"html")));
+
+
+app.get('/',function(req,res){
+    console.log("경로 : ",path.join(__dirname,"html"));
+
+    res.send('Hello World')
+});
+
+app.get('/',function(req,res){
+    res.send('apple')
+});
+
+app.get('/banana',function(req,res){
+    res.send('banana')
+});
+
+app.get('/home',function(req,res){
+    // res.send(__dirname);
+    res.sendFile(__dirname+'/home.html');
+});
+
+app.get('/grape/:name',function(req,res){
+    // res.send(__dirname);
+    console.log('path', req.path);
+    console.log('params',req.params);
+    console.log('query',req.query);
+    res.send("포도페이지입니다.");
+});
+
+app.listen(3000);
