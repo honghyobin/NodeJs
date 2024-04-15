cc4cb7cf165dc3503275404962b08fc3a16275ca
honghyobin <w2234@e-mirim.hs.kr>
2024-04-15 12:15:06 +0900

    node 04/15

--- express/index.js
+++ express/index.js
@@ -4,12 +4,33 @@ const path = require('path')
 
 app.use("/html2",express.static(path.join(__dirname,"html")));
 
+// //미들웨어 함수
+// const middle1=(req,res,next)=>{
+//     console.log("미들웨어 실행");
 
-app.get('/',function(req,res){
+//     //다음 미들웨어 또는 라우트 핸들러로 제어를 전달달
+//     next();
+// };
+
+//미들웨어 함수 모듈에서 가지고 오기
+const middle1=require("./my_modules/middle1");
+
+//미들웨어 등록
+app.use(middle1);
+
+app.get('/',function(req,res,next){
     console.log("경로 : ",path.join(__dirname,"html"));
-
-    res.send('Hello World')
+    res.send('Hello World2');
+    next();
 });
+app.get('/',function(req,res){
+    console.log("두번째/입니다");
+    res.send('Hello World2')
+});
+// app.get('/home',function(req,res){
+//     console.log("경로 : ",path.join(__dirname,"html"));
+//     res.send('home입니다')
+// });
 
 app.get('/',function(req,res){
     res.send('apple')
@@ -19,8 +40,19 @@ app.get('/banana',function(req,res){
     res.send('banana')
 });
 
-app.get('/home',function(req,res){
+app.get('/home',function(req,res,next){
+    //애플리케이션 수준의 상태 저장
+    req.app.locals.message='Hello, world~';
     // res.send(__dirname);
+    // res.sendFile(__dirname+'/home.html');
+    next();
+},(req,res)=>{
+    //응답 수준의 상태 저장
+    res.locals.additionalMessage='swag';
+    //저장된 상태 값 사용
+    const message=`${req.app.locals.message} ${res.locals.additionalMessage}`
+    console.log(message);
+
     res.sendFile(__dirname+'/home.html');
 });
 
---
+++ express/my_modules/middle1.js
@@ -0,0 +1,8 @@
+//미들웨어 함수
+const middle1=(req,res,next)=>{
+    console.log("미들웨어 모듈");
+    //다음 미들웨어 또는 라우트 핸들러로 제어를 전달달
+    next();
+};
+
+module.exports=middle1;
